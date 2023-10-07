5842fe517257de34bc54a14af2f5cbba9f851ab5
hengage <tunecarterkillz@gmail.com>
2023-10-07 02:12:14 +0100

    Feature: tailwind setupp

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/*.html',
+    'apps/*/templates/*/*.html',
+    'apps/*/templates/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
